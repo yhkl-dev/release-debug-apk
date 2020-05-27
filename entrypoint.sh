97575abc2f6932c4ce3884b3778be5355cd0127c
yhkl-dev <kaiyang939325@gmail.com>
2020-05-27 19:09:59 +0800

    init

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+config_dir=".ossutilconfig"
+
+hub checkout master
+#git branch
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE}" $(date +%Y%m%d%H%M%S) 
+
+wget http://gosspublic.alicdn.com/ossutil/1.6.13/ossutil64  
+
+chmod 755 ossutil64
+
+echo "[Credentials]" > $config_dir
+echo "language=CH" >> $config_dir
+echo "endpoint=$endpoint" >> $config_dir
+echo "accessKeyID=$accessKeyID" >> $config_dir
+echo "accessKeySecret=$accessKeySecret" >> $config_dir
+
+
