bc40335cf0e6cb3d0e23be5348e604b0713ff7be
yhkl-dev <kaiyang939325@gmail.com>
2020-06-12 21:59:26 +0800

    optimize

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 config_dir=".ossutilconfig"
+current_date=`date '+%Y%m%d_%H%M%S'`
+apk_name=$uploadDir\_$current_date.apk
 
 hub checkout master
 #git branch
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S) 
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $current_date 
  
 
 wget http://gosspublic.alicdn.com/ossutil/1.6.13/ossutil64  
@@ -18,5 +20,7 @@ echo "endpoint=$endpoint" >> $config_dir
 echo "accessKeyID=$accessKeyID" >> $config_dir
 echo "accessKeySecret=$accessKeySecret" >> $config_dir
 
-./ossutil64 cp ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk oss://$uploadDir/$uploadDir\_`date '+%Y%m%d_%H%M%S'`.apk -c $config_dir
+mv ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk  ./${APP_FOLDER}/build/outputs/apk/debug/$apk_name
+
+./ossutil64 cp ./${APP_FOLDER}/build/outputs/apk/debug/$apk_name oss://$uploadDir/$apk_name -c $config_dir
 
